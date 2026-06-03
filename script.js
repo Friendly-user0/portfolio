460c6693fa8e3c61e51430aef77e98025a1bb71e
Friendly-user0 <nishthapa09@gmail.com>
2026-06-03 08:35:33 +0545

    Redesiging haha

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("Portfolio loaded");
+const roles = [
+  "Security Researcher",
+  "AI Security Researcher",
+  "Vulnerability Analyst",
+  "OSINT Explorer"
+];
+
+let i = 0;
+let j = 0;
+let current = "";
+let isDeleting = false;
+
+function type() {
+  current = roles[i];
+
+  if (!isDeleting) {
+    document.getElementById("typing").textContent =
+      current.substring(0, j++);
+  } else {
+    document.getElementById("typing").textContent =
+      current.substring(0, j--);
+  }
+
+  if (j === current.length + 1) {
+    isDeleting = true;
+    setTimeout(type, 1000);
+    return;
+  }
+
+  if (j === 0) {
+    isDeleting = false;
+    i = (i + 1) % roles.length;
+  }
+
+  setTimeout(type, isDeleting ? 50 : 80);
+}
+
+type();
