334fb83a44be726bcf0173fd27f54bff7beb0ed7
Friendly-user0 <nishthapa09@gmail.com>
2026-06-03 08:56:08 +0545

    Redisigning again

--- script.js
+++ script.js
@@ -1,38 +1,12 @@
-const roles = [
-  "Security Researcher",
-  "AI Security Researcher",
-  "Vulnerability Analyst",
-  "OSINT Explorer"
-];
+// scroll animation
+const items = document.querySelectorAll(".reveal");
 
-let i = 0;
-let j = 0;
-let current = "";
-let isDeleting = false;
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
+});
 
-function type() {
-  current = roles[i];
-
-  if (!isDeleting) {
-    document.getElementById("typing").textContent =
-      current.substring(0, j++);
-  } else {
-    document.getElementById("typing").textContent =
-      current.substring(0, j--);
-  }
-
-  if (j === current.length + 1) {
-    isDeleting = true;
-    setTimeout(type, 1000);
-    return;
-  }
-
-  if (j === 0) {
-    isDeleting = false;
-    i = (i + 1) % roles.length;
-  }
-
-  setTimeout(type, isDeleting ? 50 : 80);
-}
-
-type();
+items.forEach(el => observer.observe(el));
